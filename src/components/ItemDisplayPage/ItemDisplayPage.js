00608001e8e37f04dd19e0c4c4db3aa3390b9a34
Jeffrey L McMahon <jeffmcmahon1973@gmail.com>
2020-12-03 16:13:43 -0600

    item details page OK

--- src/components/ItemDisplayPage/ItemDisplayPage.js
+++ src/components/ItemDisplayPage/ItemDisplayPage.js
@@ -41,26 +41,26 @@ class ItemDisplayPage extends Component {
             return (
               <>
                 <div class="container-fluid" id='thebox'>
-                  <div class="row">
-                    <div class="col-lg">
+                  
+                    <div  class="col-lg" id='columnIDP'>
                       <img id='imageIDP' src={`/media/${item.item_image}`} />
                     </div>
-                    <div class="col-lg">
+                    <div class="col-lg" id='columnIDP'>
                       <table id='itemTable'>
                         <th><td></td><td></td></th>
-                        <tr><td id='heading'>Item Name:</td><td>{item.item_name}</td></tr>
-                        <tr><td id='heading'>Model Number:</td><td>{item.item_model}</td></tr>
-                        <tr><td id='heading'>Notable Details:</td><td>{item.item_detail}</td></tr>
-                        <tr><td id='heading'>Location:</td><td>{item.item_location}</td></tr>
-                        <tr><td id='heading'>Price: $</td><td>{item.item_price}</td></tr>
+                        <tr><td id='heading'>Item Name:</td><td id='heading2' >{item.item_name}</td></tr>
+                        <tr><td id='heading'>Model Number:</td><td id='heading2'>{item.item_model}</td></tr>
+                        <tr><td id='heading'>Notable Details:</td><td id='heading2'>{item.item_detail}</td></tr>
+                        <tr><td id='heading'>Location:</td><td id='heading2'>{item.item_location}</td></tr>
+                        <tr><td id='heading'>Price: $</td><td id='heading2'>{item.item_price}</td></tr>
                       </table>
                     </div>
-                    <div class="col-lg">
-                      <button id='deletebutton' onClick={() => this.removeItem(item.id)}>Delete</button>
-                      <button id='forSaleButton' onClick={() => this.editItem(item.id)}>Add To For Sale List</button>
-                      <button id='wishListButton' onClick={() => this.editItem(item.id)}>Add To Wish List</button>
+                    <div class="col-lg" id='columnIDP'>
+                      <div><button id='deletebutton' onClick={() => this.removeItem(item.id)}>Delete</button></div>
+                      <div><button id='forSaleButton' onClick={() => this.editItem(item.id)}>Add To For Sale List</button></div>
+                      <div><button id='wishListButton' onClick={() => this.editItem(item.id)}>Add To Wish List</button></div>
                     </div>
-                  </div>
+                  
                   
                 </div>
               </>
